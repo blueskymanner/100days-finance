e3387b5818cbb132f7b232230b028888b6f915cd
blueskymanner <page111first@gmail.com>
2022-08-22 01:42:22 +0200

    added 2 folders

---
+++ src/_common/address.js
@@ -0,0 +1,5 @@
+// export const astroTokenAddress = '0xA1344abad6CBd7943deC5d12c2BFbA22878426bA';
+// export const astroTokenAddress = '0x6007FEC5ad4718dD899F8405bc80284eFF860dA5';
+export const astroTokenAddress = '0x081A99648fA7e9F0A8839715f8068a5BDc106312';
+export const usdcTokenAddress = '0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E';
+export const wavaxTokenAddress = '0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7';
